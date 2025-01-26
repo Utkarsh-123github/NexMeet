abc28a3409d4c606ad9696fbb0d7b9d349bbf7d3
Utkarsh Tiwari <utk0703@gmail.com>
2025-01-26 12:12:26 +0530

    initial commit of the NexMeet app

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "NexMeet",
+  description: "A free and seamless video conferencing platform",
+  icons: '/icons/logo.svg',
+};
+
+const RootLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
